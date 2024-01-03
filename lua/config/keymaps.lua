3db3b334f0136cd8ecf098241d2384847db137e7
Nate The Ok <nnyquist@thinkbrg.com>
2024-01-03 12:48:55 -0500

    added toggleterm
    * keymap to toggle terminal
    * keymap to escape from term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -14,16 +14,21 @@ keymap.set("n", "<C-k>", "<C-w>k", opts("Navigate Up"))
 keymap.set("n", "<C-l>", "<C-w>l", opts("Navigate Right"))
 
 -- Window Management
-keymap.set("n", "<leader>wv", ":vsplit<CR>", opts("[W]indow [V]ertical Split"))
-keymap.set("n", "<leader>wh", ":split<CR>", opts("[W]indow [H]orizontal Split"))
-keymap.set("n", "<leader>wq", "<C-w>q", opts("[W]indow [Q]uit"))
-keymap.set("n", "<leader>wo", "<C-w>o", opts("[W]indow [O]nly"))
+local prefix = "<leader>w"
+keymap.set("n", prefix .. "v", ":vsplit<CR>", opts("[W]indow [V]ertical Split"))
+keymap.set("n", prefix .. "h", ":split<CR>", opts("[W]indow [H]orizontal Split"))
+keymap.set("n", prefix .. "q", "<C-w>q", opts("[W]indow [Q]uit"))
+keymap.set("n", prefix .. "o", "<C-w>o", opts("[W]indow [O]nly"))
 
 -- Let Ctlr-S save the current buffer
 keymap.set("n", "<C-s>", ":w<CR>", opts("Save Buffer"))
 
 -- Quit
 keymap.set("n", "<leader>q", ":q<CR>", opts("[Q]uit"))
+keymap.set("n", "<leader>Q", ":q!<CR>", opts("Force [Q]uit"))
+
+-- Terminal Bindings
+keymap.set("t", "<Esc>", "<C-\\><C-n>", opts("Escape Terminal"))
 
 -- [[ Source: ThePrimeagen ]]
 -- Move Selected Lines
