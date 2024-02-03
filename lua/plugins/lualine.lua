7c60f01ca70453d27c1a0fe9dadbd18329b4fdfb
Nathan Nyquist <nnyquist@thinkbrg.com>
2024-02-02 21:44:59 -0500

    show recording macro in statusline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,23 +1,30 @@
 local config = function()
-    local theme = require("lualine.themes.palenight")
-    theme.normal.c.gb = nil
+	local theme = require("lualine.themes.palenight")
+	theme.normal.c.gb = nil
 
-    require("lualine").setup {
-        options = {
-            theme = theme,
-            globalstatus = true,
-        },
-        sections = {
-            lualine_a = {
-                { "buffers" },
-            },
-        },
-    }
+	require("lualine").setup({
+		options = {
+			theme = theme,
+			globalstatus = true,
+		},
+		sections = {
+			lualine_a = {
+				{ "buffers" },
+			},
+			lualine_x = {
+				{
+					require("noice").api.statusline.mode.get,
+					cond = require("noice").api.statusline.mode.has,
+					color = { fg = "#ff9e64" },
+				},
+			},
+		},
+	})
 end
 
 return {
-    'nvim-lualine/lualine.nvim',
-    lazy = false,
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    config = config,
+	"nvim-lualine/lualine.nvim",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = config,
 }
