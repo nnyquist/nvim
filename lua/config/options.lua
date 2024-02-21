706e5f05931ec00e0f3b3a3907653348a64b13fe
Nate The Ok <nnyquist@thinkbrg.com>
2024-02-21 14:18:03 -0500

    check current os in options.lua
    if Windows, use PowerShell; otherwise, use default

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,16 +39,20 @@ opt.clipboard:append("unnamedplus")
 -- Completion
 vim.o.completeopt = "menuone,noselect"
 
--- Powershell
-local powershell_options = {
-	shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
-	shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-	shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-	shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-	shellquote = "",
-	shellxquote = "",
-}
+-- Powershell (if in a Windows OS)
+local uname = vim.loop.os_uname()
 
-for option, value in pairs(powershell_options) do
-	vim.opt[option] = value
+if uname.sysname == "Windows_NT" then
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
 end
